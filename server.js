a1b5048690478ace1d2a703e764b734768a1a751
ItsGodHere <shouro-agoro@et.esiea.fr>
2022-01-06 15:51:56 +0100

    Setup project

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var serve_static = require("serve-static");
+var http = require("http");
+var fs = require("fs");
+
+var app = express();
+//Activation du serveur statique
+app.use(serve_static(__dirname + "/public"));
+
+//Récupération du serveur http de l'application
+var serveur = http.Server(app);
+
+//Ecoute sur un seul port
+serveur.listen(8080, function () {
+  console.log("Serveur en écoute sur le port 8080");
+});
