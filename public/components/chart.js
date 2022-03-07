c084edb63cf16c318541ac1bc9a08a40e49d6caa
ItsGodHere <shouro-agoro@et.esiea.fr>
2022-03-07 05:02:31 +0100

    Refactoring index.html, weather.js & add chart.js

---
+++ public/components/chart.js
@@ -0,0 +1,65 @@
+var weatherChart = {
+  components: {
+    apexchart: VueApexCharts,
+  },
+  props: ["serie"],
+  data: function () {
+    return {
+      chartOptions: {
+        chart: {
+          id: "realtime",
+          type: "line",
+          animations: {
+            enabled: true,
+            easing: "linear",
+            dynamicAnimation: {
+              speed: 1000,
+            },
+          },
+          toolbar: {
+            show: false,
+          },
+          zoom: {
+            enabled: false,
+          },
+        },
+        dataLabels: {
+          enabled: false,
+        },
+        stroke: {
+          curve: "smooth",
+        },
+        markers: {
+          size: 0,
+        },
+        xaxis: {
+          labels: {
+            show: false,
+          },
+          range: 60,
+        },
+        yaxis: {},
+        legend: {
+          show: false,
+        },
+      },
+    };
+  },
+  mounted: function () {
+    setInterval(() => {
+      var me = this;
+      me.$refs.chart.updateSeries([
+        {
+          data: this.serie[0].data,
+        },
+      ]);
+    }, 1000);
+  },
+  template: `
+  <div id="realtime">
+    <apexchart type="line" ref="chart" 
+      :options="chartOptions" :series="this.serie">
+    </apexchart>
+  </div>
+  `,
+};
--- public/components/weather.js
+++ public/components/weather.js
@@ -4,7 +4,7 @@ var weatherCard = {
     return {};
   },
   template: `
-    <v-card elevation="20" loading width="100%">
+    <v-card elevation="5" width="100%">
       <v-container class="pa-2 text-center" justify="center">
         <span class="">{{coordinates[0]}}</span>
         <span class=""> | </span>
@@ -18,36 +18,36 @@ var weatherCard = {
           <v-col>
             <v-container class="text-no-wrap rounded-pill">
               <div>
-                <v-avatar>
+                <v-avatar size="45">
                   <v-img src="/icons/barometer.png"
                     alt="Barometer icon created by Freepik - Flaticon"></v-img>
                 </v-avatar>
               </div>
-              <div>{{weather_data.pressure}} Pa</div>
+              <div class="font-weight-black">{{weather_data.pressure}} Pa</div>
               <div class="text-uppercase font-weight-light text-caption">Pressure</div>
             </v-container>
           </v-col>
           <v-col>
                 <v-container class="text-no-wrap rounded-pill">
                   <div>
-                    <v-avatar>
+                    <v-avatar size="45">
                       <v-img src="/icons/raindrop.png" 
                         alt="Water icons created by Freepik - Flaticon.com"></v-img>
                     </v-avatar>
                   </div>
-                  <div>{{weather_data.hygrometry}} %</div>
+                  <div class="font-weight-black">{{weather_data.hygrometry}} %</div>
                   <div class="text-uppercase font-weight-light text-caption">Humidity</div>
                 </v-container>
           </v-col>
           <v-col>
                 <v-container class="text-no-wrap rounded-pill">
                   <div>
-                  <v-avatar>
+                  <v-avatar size="45">
                     <v-img src="/icons/low-temperature.png"
                       alt="Weather icons created by Freepik - Flaticon"></v-img>
                   </v-avatar>
                 </div>
-                  <div>{{weather_data.temperature_inside}}&deg C</div>
+                  <div class="font-weight-black">{{weather_data.temperature_inside}}&deg C</div>
                   <div class="text-uppercase font-weight-light text-caption">Feel Like</div>
                 </v-container>
           </v-col>
